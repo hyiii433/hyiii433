448628be67fdb4176798f8dcaa044cc5a009c1fd
hyiii433 <2066570397@qq.com>
2025-03-08 18:36:04 +0800

    处理文件的主程序 进行过代码质量检查

--- 3223004296/main.py
+++ 3223004296/main.py
@@ -1,8 +1,9 @@
 import sys
 from similarity import cosine_similarity
 
+
 def read_file(filepath):
-    """读取文件内容并返回文本"""
+    # f读取文件内容并返回文本
     try:
         with open(filepath, "r", encoding="utf-8") as file:
             return file.read()
@@ -10,16 +11,20 @@ def read_file(filepath):
         print(f"文件 {filepath} 未找到")
         return None
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python main.py <原文文件> <抄袭版文件> <答案文件>")
         sys.exit(1)
 
-    orig_file, plagiarized_file, output_file = sys.argv[1], sys.argv[2], sys.argv[3]
+# 获取文件路径
+    orig_file, plagiarized_file, output_file \
+        = sys.argv[1], sys.argv[2], sys.argv[3]
 
     text1 = read_file(orig_file)
     text2 = read_file(plagiarized_file)
 
+# 计算原文与抄袭文相似度similarity
     similarity = cosine_similarity(text1, text2)
 
     with open(output_file, 'w', encoding='utf-8') as file:
